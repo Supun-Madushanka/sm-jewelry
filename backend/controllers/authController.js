9f638c5dba00f1dc351c1e747a027816d905f4b7
Supun Madushanka <supunmadushanka2582000@gmail.com>
2025-04-24 17:32:14 +0530

    complete sign up api

---
+++ backend/controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require("../models/userModel")
+const bcryptjs = require('bcryptjs')
+const errorHandler = require("../utils/error")
+const jwt = require('jsonwebtoken')
+
+const signup = async (req, res, next) => {
+    const { username, email, password } =  req.body
+
+    if(!username || !email || !password || username === '' || email === '' || password === ''){
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    try {
+        await newUser.save()
+        res.json('Signup successful')
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    signup,
+}
--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,13 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/authRoutes')
 
 const app = express()
 
 app.use(express.json())
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+
+app.use('/api/v1/auth', authRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
