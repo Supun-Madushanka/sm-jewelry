d40d4bffec0c422893cd44af332cad58db4a0342
Supun Madushanka <supunmadushanka2582000@gmail.com>
2025-04-24 17:17:59 +0530

    create user model & handling error function

--- backend/index.js
+++ backend/index.js
@@ -20,3 +20,13 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.log(error)
     })
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
