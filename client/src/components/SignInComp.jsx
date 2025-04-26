f2aee795b94b7d734fa74563447eeca1d928d3e5
Supun Madushanka <supunmadushanka2582000@gmail.com>
2025-04-26 20:42:03 +0530

    complete sign in functionality

--- client/src/components/SignInComp.jsx
+++ client/src/components/SignInComp.jsx
@@ -1,11 +1,59 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function SignInComp() {
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/v1/auth/signin',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+
+      if(res.ok) {
+        navigate('/');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
     return (
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
         <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-lg">
           <h2 className="text-2xl font-semibold text-center text-black mb-6">Sign In</h2>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="mb-4">
               <label htmlFor="email" className="block text-sm font-medium text-gray-900">Email</label>
               <input
@@ -13,6 +61,8 @@ export default function SignInComp() {
                 id="email"
                 className="w-full mt-2 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Enter your email"
+                onChange={handleChange}
+                required
               />
             </div>
             
@@ -23,14 +73,23 @@ export default function SignInComp() {
                 id="password"
                 className="w-full mt-2 px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Enter your password"
+                onChange={handleChange}
+                required
               />
             </div>
             
             <button
               type="submit"
+              disabled={loading}
               className="w-full py-2 px-4 bg-black text-white rounded-lg hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
             >
-              Sign In
+              {loading ? (
+                 <>
+                   <span className='pl-3'>Loading...</span>
+                 </>
+               ) : (
+                 'Sign In'
+               )}
             </button>
             
             <div className="mt-4 text-center text-sm">
@@ -39,6 +98,7 @@ export default function SignInComp() {
                 Sign up
               </Link>
             </div>
+            {errorMessage && <p className="text-red-500 text-center mt-2">{errorMessage}</p>}
           </form>
         </div>
       </div>
