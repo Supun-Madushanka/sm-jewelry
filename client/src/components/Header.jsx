e9bff5ce52f73705c1673ca17045ae404042bb99
Supun Madushanka <supunmadushanka2582000@gmail.com>
2025-04-30 17:55:13 +0530

    complete sign out in frontend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { Link, useLocation } from 'react-router-dom';
 import { Menu } from 'lucide-react';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch  } from 'react-redux';
+import { signoutSuccess } from '../redux/user/userSlice'
 
 export default function Header() {
   const location = useLocation();
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
 
   const navLinks = [
     { name: 'Home', path: '/' },
@@ -15,6 +16,24 @@ export default function Header() {
     { name: 'Contact', path: '/contact' },
     { name: 'Collections', path: '/collections'}
   ];
+
+  const handleSignOut = async() => {
+    try {
+      const res = await fetch('/api/v1/user/signout', {
+        method: 'POST'
+      })
+
+      const data = await res.json()
+
+      if(!res.ok) {
+        console.log(data.message);
+      }else{
+        dispatch(signoutSuccess())
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   return (
     <header className="flex items-center justify-between shadow-md py-4 px-4 sm:px-10 bg-white min-h-[70px] tracking-wide relative z-50">
@@ -47,7 +66,7 @@ export default function Header() {
           <>
             <button
               className='text-[15px] px-4 py-2 text-sm cursor-pointer rounded-md font-medium bg-white text-black border border-gray-900 hover:bg-black hover:text-white transition'
-              onClick={() => {}}
+              onClick={handleSignOut}
             >
               Sign Out
             </button>
